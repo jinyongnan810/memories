0309571de2350ca8a380a12384dc642047b7a4aa
Kin Yuunan <yuunan.kin@gmail.com>
2024-08-13 09:35:13 +0900

    select time range

--- lib/pages/add_memory_page.dart
+++ lib/pages/add_memory_page.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:memories/providers/memories.dart';
+import 'package:omni_datetime_picker/omni_datetime_picker.dart';
 
 import '../providers/providers.dart';
+
+final DateFormat formatter = DateFormat('yyyy/MM/dd HH:mm');
 
 class AddMemoryPage extends HookConsumerWidget {
   const AddMemoryPage({super.key});
@@ -13,6 +17,10 @@ class AddMemoryPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final location = useState<GeoPoint>(const GeoPoint(0, 0));
+    final startDateTime = useState<DateTime?>(null);
+    final endDateTime = useState<DateTime?>(null);
+    final titile = useState<String>('');
+    final contents = useState<String>('');
     useEffect(
       () {
         Future(() {
@@ -52,10 +60,70 @@ class AddMemoryPage extends HookConsumerWidget {
             ),
           ],
         ),
-        body: const Center(
-          child: Text('Add Memory'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _PickDateArea(
+                  startDateTime: startDateTime,
+                  endDateTime: endDateTime,
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _PickDateArea extends StatelessWidget {
+  const _PickDateArea({required this.startDateTime, required this.endDateTime});
+  final ValueNotifier<DateTime?> startDateTime;
+  final ValueNotifier<DateTime?> endDateTime;
+  @override
+  Widget build(BuildContext context) {
+    final start = startDateTime.value;
+    final end = endDateTime.value;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () async {
+            final dateTimeList = await showOmniDateTimeRangePicker(
+              context: context,
+              barrierDismissible: true,
+              constraints: const BoxConstraints(maxWidth: 400),
+              startWidget: const Text('開始日時'),
+              endWidget: const Text('終了日時'),
+              isForceEndDateAfterStartDate: true,
+              onStartDateAfterEndDateError: () {
+                Navigator.of(context).pop();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('開始日時は終了日時より前に設定してください。'),
+                  ),
+                );
+              },
+            );
+            if (dateTimeList != null) {
+              startDateTime.value = dateTimeList[0];
+              endDateTime.value = dateTimeList[1];
+            }
+          },
+          label: const Text('思い出の期間を選択'),
+          icon: const Icon(Icons.calendar_today),
+        ),
+        if (start != null && end != null) ...[
+          const SizedBox(height: 8),
+          Text(
+            '${formatter.format(start)} ~ ${formatter.format(end)}',
+          ),
+        ],
+      ],
     );
   }
 }
