696ea523099d5c7126dee4978ce36c3a6f952382
Kin Yuunan <yuunan.kin@gmail.com>
2024-08-25 16:47:14 +0900

    loading

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memories/components/overlays/menu_overlay_wrapper.dart';
+import 'package:memories/providers/memories.dart';
 
 import '../components/components.dart';
 
@@ -26,10 +27,46 @@ class Home extends ConsumerWidget {
             ],
           ),
           body: Stack(
-            children: [Positioned.fill(child: MapView())],
+            children: [
+              Positioned.fill(child: MapView()),
+              const Positioned.fill(child: _Loading()),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Loading extends ConsumerWidget {
+  const _Loading();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(memoriesProvider).isLoading;
+    if (!isLoading) {
+      return const SizedBox.shrink();
+    }
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Container(
+            color: Colors.black.withOpacity(0.5),
+            alignment: Alignment.center,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(height: 12),
+                Text(
+                  '思い出を読み込んでいます...',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
