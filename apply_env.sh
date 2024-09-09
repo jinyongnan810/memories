f79495b3f4fe1310e37be0c885845d82bc47c55a
Kin Yuunan <yuunan.kin@gmail.com>
2024-09-09 11:19:23 +0900

    update apply env

--- apply_env.sh
+++ apply_env.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
 # Step 1: Extract the API key from the env file
 API_KEY=$(grep 'GOOGLE_MAP_API_KEY' env | cut -d '=' -f2)
 
-# Step 2: Replace the placeholder in index.html
-# Note: If you're using macOS, add '' after -i in the sed command
-sed -i "" "s/YOUR_API_KEY/${API_KEY}/g" web/index.html
+# Step 2: Detect the operating system
+OS_TYPE=$(uname)
+
+# Step 3: Set the sed command based on the OS
+if [ "$OS_TYPE" = "Darwin" ]; then
+    # macOS
+    SED_COMMAND="sed -i '' \"s/YOUR_API_KEY/${API_KEY}/g\" web/index.html"
+else
+    # Linux (Ubuntu)
+    SED_COMMAND="sed -i \"s/YOUR_API_KEY/${API_KEY}/g\" web/index.html"
+fi
+
+# Step 4: Execute the sed command
+eval $SED_COMMAND
